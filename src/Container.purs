de383f6350f4cd74e06fa51600aae99b18f2e29a
John Watson <john.watson@gmx.co.uk>
2019-04-19 18:54:39 +0100

    Recognise mouse click positions on the canvas
    relative to the position of the canvas element itself (not the entire
    screen).

--- src/Container.purs
+++ src/Container.purs
@@ -2,31 +2,47 @@ module Container where
 
 import Prelude
 import Effect.Aff (Aff)
+import Halogen.Aff as HA
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Halogen.HTML.Core (ClassName(..))
+import Web.UIEvent.MouseEvent (MouseEvent, clientX, clientY)
+import Web.HTML.HTMLElement (offsetTop, offsetLeft)
+import Web.DOM.ParentNode (QuerySelector(..))
 import Graphics.Canvas (Context2D, CanvasElement, getCanvasElementById, getContext2D)
 import Graphics.Drawing (render) as Drawing
 import Data.Maybe (Maybe(..), fromJust)
 import Partial.Unsafe (unsafePartial)
+import Data.Int (toNumber)
 import Graphics (canvasHeight, canvasWidth, chordDisplay)
 import Export (exportAs)
 import Types (ExportFormat(..), toMimeType)
 
+import Debug.Trace (spy)
+
+type CanvasPosition =
+  { left :: Number
+  , top  :: Number
+  }
 
 type State =
   { -- mAudioContext :: Maybe AudioContext
     mGraphicsContext :: Maybe Context2D
   , mCanvas :: Maybe CanvasElement
+  , canvasPosition :: CanvasPosition
   }
 
 data Action =
     Init
+  | EditFingering Int Int
   | Export ExportFormat
 
-component :: ∀ i o q. H.Component HH.HTML q i o Aff
+data Query a =
+  GetCanvasOffset a
+
+component :: ∀ i o. H.Component HH.HTML Query i o Aff
 component =
   H.mkComponent
     { initialState
@@ -44,6 +60,7 @@ component =
     { -- mAudioContext : Nothing
       mGraphicsContext : Nothing
     , mCanvas : Nothing
+    , canvasPosition : { left : 0.0, top : 0.0 }
     }
 
   render :: State -> H.ComponentHTML Action () Aff
@@ -54,6 +71,7 @@ component =
          [HH.text "Guitar Chord Builder" ]
       , HH.canvas
          [ HP.id_ "canvas"
+         , HE.onClick canvasClickHandler
          , HP.height canvasHeight --300
          , HP.width  canvasWidth -- 300
          ]
@@ -90,6 +108,15 @@ component =
       -- _ <- H.liftEffect $ Export.canvasToDataURL canvas "image/png"
       _ <- H.modify (\st -> st { mGraphicsContext = Just graphicsCtx
                                , mCanvas = mCanvas })
+      _ <- handleQuery (GetCanvasOffset unit)
+      pure unit
+    EditFingering cx cy -> do
+      state <- H.get
+      let
+        x = toNumber cx - state.canvasPosition.left
+        y = toNumber cy - state.canvasPosition.top
+        foo = spy "X:" x
+        bar = spy "Y:" y
       pure unit
     Export format -> do
       state <- H.get
@@ -98,3 +125,25 @@ component =
         mimeType = toMimeType format
       _ <- H.liftEffect $ exportAs canvas "sampleexport" mimeType
       pure unit
+
+  handleQuery :: ∀ o a. Query a -> H.HalogenM State Action () o Aff (Maybe a)
+  handleQuery = case _ of
+    -- get the coordinates of the upper left hand corner of the canvas we've
+    -- just built.  We need this to find accurate mouse click references relative
+    -- to the canvas itsef (not the entire screen). I think it is OK to use DOM
+    -- here because we only call this once immediately after initialising the canvas
+    GetCanvasOffset next -> do
+      mCanvasElement <- H.liftAff $ HA.selectElement (QuerySelector "#canvas")
+      let
+        canvasElement = unsafePartial (fromJust mCanvasElement)
+      left <- H.liftEffect $ offsetLeft canvasElement
+      top <- H.liftEffect $ offsetTop canvasElement
+      let
+        foo = spy "Left:" left
+        bar = spy "Top:" top
+      _ <- H.modify (\st -> st { canvasPosition  = { left, top } })
+      pure (Just next)
+
+  canvasClickHandler :: MouseEvent -> Maybe Action
+  canvasClickHandler me =
+    Just $ EditFingering (clientX me) (clientY me)
